c75cdf8671084a5a40889162b734cc0dd0d8960f
Karla Díaz <karladiazsaura@gmail.com>
2024-10-11 14:05:37 -0400

    init

--- obj/Debug/net8.0/Gafas2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gafas2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gafas2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+779687d2cb9574f5b1b5cb103cf367eb8b954562")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gafas2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gafas2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
